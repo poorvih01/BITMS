2fcae53eaf9ccab793544ef1e74216b549b135e9
poorvih01 <167217548+poorvih01@users.noreply.github.com>
2024-04-26 06:52:52 +0530

    Add files via upload

---
+++ fizzzbuzzz task.py
@@ -0,0 +1,9 @@
+for i in range(0,100):
+    if i%5==0 and i%3==0:
+        print("fizzbuzzz")
+    elif i%3==0:
+        print("buzzz")
+    elif i%5==0:
+        print("fizzz")
+    else:
+        print(i)
